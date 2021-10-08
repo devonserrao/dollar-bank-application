7fa85af8aac738d3fd5385b05f21cbcd6b014d44
devonserrao <devonserrao101@gmail.com>
2021-10-08 12:22:22 -0500

    Added account class with details to create accounts linked to specific customers

---
+++ src/Account.js
@@ -0,0 +1,9 @@
+class Account {
+
+    constructor(id, accountType, balance, customerId) {
+        this.id = id;
+        this.accountType = accountType;
+        this.balance = balance;
+        this.customerId = customerId;
+    }
+}
