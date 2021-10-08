39ca3504dd1d379c5d1a25206ef817543e2c5ed7
devonserrao <devonserrao101@gmail.com>
2021-10-08 11:53:45 -0500

    Added Customer js file with constructor and functions to be added for it

---
+++ src/Customer.js
@@ -0,0 +1,9 @@
+class Customer {
+
+    constructor(id, name, accountType, balance) {
+        this.id = id;
+        this.name = name;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+}
